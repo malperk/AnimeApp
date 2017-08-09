1c393d39f2bfab352a96dae1fe37b21363985202
Alper KARATAŞ <AlperKaratas@kkb.com.tr>
2017-08-09 12:16:19 +0300

    Cleaning and adding first screen

---
+++ AnimeApp/BaseClasses/VCBase.swift
@@ -0,0 +1,12 @@
+//
+//  VCBase.swift
+//  AnimeApp
+//
+//  Created by Alper KARATAŞ on 09/08/2017.
+//  Copyright © 2017 Alper KARATAŞ. All rights reserved.
+//
+
+import UIKit
+
+class VCBase: UIViewController {
+}
